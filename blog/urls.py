ddd17e0348740d6d7006af631c4dc88ce7da1fcb
伊東佳樹 <ito@itouyoshikinoMacBook-Air.local>
2020-01-27 15:25:57 +0900

    add facicon.ico

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name='post_list',name='top'),
+    path('', views.post_list, name='post_list'),
     path('recommend/pattern1', views.read_vec50, name='vec50'),
     path('recommend/pattern2', views.read_vec60, name='vec60'),
 ]
