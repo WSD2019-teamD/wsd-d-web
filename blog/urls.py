85fe4f1fd062e39019be4dcec19dba25637533e1
伊東佳樹 <ito@itouyoshikinoMacBook-Air.local>
2020-01-27 15:48:45 +0900

    change pattern

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('recommend/pattern1', views.read_vec50, name='vec50'),
-    path('recommend/pattern2', views.read_vec60, name='vec60'),
+    path('recommend/pattern2', views.read_vec50, name='vec50'),
+    path('recommend/pattern1', views.read_vec60, name='vec60'),
 ]
