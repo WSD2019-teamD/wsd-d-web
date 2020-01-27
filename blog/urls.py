9dd1687812e7dd0517b56419eaaa9fd75a27edcc
伊東佳樹 <ito@itouyoshikinoMacBook-Air.local>
2020-01-27 15:03:03 +0900

    cahnge url

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('recommend/lfhdja', views.read_vec50, name='vec50'),
-    path('recommend/khfajgd', views.read_vec60, name='vec60'),
+    path('', views.post_list, name='post_list',name='top'),
+    path('recommend/pattern1', views.read_vec50, name='vec50'),
+    path('recommend/pattern2', views.read_vec60, name='vec60'),
 ]
